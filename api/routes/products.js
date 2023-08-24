fe7ee32fc817a5f38c0a779e5fe8ea03af75eb86
Nob121 <tahmidambrose.420@gmail.com>
2023-08-23 21:20:20 -0700

    Added more routes to the products and orders

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,44 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/');
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    })
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    })
+});
 
 
+router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Special ID is functional now',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Wrong ID',
+        })
+    }
+})
+
+router.patch('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated Product',
+    });
+})
+
+router.delete('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted Product',
+    });
+})
+
+module.exports = router;
 
